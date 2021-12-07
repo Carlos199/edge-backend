b51293cfd0d4f4b248a6777c99a9239e2d84794a
Luis Ortellado <luiscarlosortecabral@gmail.com>
2021-12-07 08:04:05 -0300

    Added encryption of password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,13 +5,12 @@ const User = require('../models/User')
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
-  console.log(req.body)
+  const data = req.body
   try {
-    const user = await User.create(req.body)
-    console.log(user)
+    const user = await User.create(data)
     return res.send({ user })
   } catch (err) {
-    return res.status(400).send({ error: 'Registration failed' })
+    return res.status(400).send({ errror: 'Registration failed' })
   }
 })
 
